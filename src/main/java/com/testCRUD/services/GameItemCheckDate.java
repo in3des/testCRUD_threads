9db99e99846e1c602a87a2563758d7c8c04c4109
in3des <62700347+in3des@users.noreply.github.com>
2021-09-15 12:36:29 +0400

    gameItemStatusUpdate added

--- src/main/java/com/testCRUD/services/GameItemCheckDate.java
+++ src/main/java/com/testCRUD/services/GameItemCheckDate.java
@@ -19,7 +19,7 @@ public class GameItemCheckDate {
     private static String currentDate = "01-01-2011";
 
 
-    public void gameItemCheckDate(GameItemServices gameItemServices) {
+    public List<GameItem> gameItemCheckDate(GameItemServices gameItemServices) {
         List<GameItem> checkList = gameItemServices.listAll();
         System.out.println("<<< --- Check Date results: --- >>>");
         List<GameItem> updateList = checkList.parallelStream()
@@ -28,10 +28,20 @@ public class GameItemCheckDate {
                 .collect(Collectors.toList());
         System.out.println(updateList);
         System.out.println("<<< --- Check Date END --- >>>");
+        return updateList;
+    }
+
+    public void gameItemStatusUpdate(GameItemServices gameItemServices) {
+        List<GameItem> updateList = gameItemCheckDate(gameItemServices);
+        for (GameItem gameItem: updateList) {
+            gameItem.setStatus(Status.RELEASE);
+            gameItemServices.save(gameItem);
+        }
+        System.out.printf("<<< --- Status successfully updated for %d GameItems --- >>>", updateList.size());
     }
 
     @PostConstruct
     private void checkTestGameItem() {
-        gameItemCheckDate(gameItemServices);
+        gameItemStatusUpdate(gameItemServices);
     }
 }
